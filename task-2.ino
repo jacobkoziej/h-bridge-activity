caa7d3ceb662ccf7603a492d81e71c3f23b5188a
Jacob Koziej <jacobkoziej@gmail.com>
2020-10-12 20:39:50 -0400

    Small refactor

--- task-2.ino
+++ task-2.ino
@@ -2,9 +2,15 @@
  * Jacob Koziej
  * H-Bridge Activity (Task 2)
  * 2020-10-08
- * 2020-10-08
+ * 2020-10-12
  */
-#define SIZEOF(X, Y) (sizeof(X) / sizeof(Y))
+
+
+// Constants
+#define Q0 9
+#define Q1 10
+
+#define DELAY 2000
 
 
 // Function prototypes
@@ -14,48 +20,46 @@ void brake(void);
 void coast(void);
 
 
-// Global variables
-const uint8_t h_bridge_q[] = {9, 10};
-
-
 // BEGIN
 void setup()
 {
-	for (int i = 0; i < SIZEOF(h_bridge_q, uint8_t); i++) {
-		pinMode(h_bridge_q[i], OUTPUT);
-	}
+	pinMode(Q0, OUTPUT);
+	pinMode(Q1, OUTPUT);
 }
 
 void loop()
 {
 	cw(128);
-	delay(2000);
+	delay(DELAY);
+	coast();
+	delay(DELAY);
 	ccw(128);
-	delay(2000);
+	delay(DELAY);
+	brake();
+	delay(DELAY);
 }
 
 // Function declarations
 void cw(uint8_t speed)
 {
-	digitalWrite(h_bridge_q[0], HIGH);
-	analogWrite(h_bridge_q[1], ~speed);
+	digitalWrite(Q0, HIGH);
+	analogWrite(Q1, ~speed);
 }
 
 void ccw(uint8_t speed)
 {
-	digitalWrite(h_bridge_q[1], HIGH);
-	analogWrite(h_bridge_q[0], ~speed);
+	digitalWrite(Q0, HIGH);
+	analogWrite(Q1, ~speed);
 }
 
 void brake(void)
 {
-	// This will destroy circuit
-	digitalWrite(h_bridge_q[0], LOW);
-	digitalWrite(h_bridge_q[1], LOW);
+	digitalWrite(Q0, LOW);
+	digitalWrite(Q1, LOW);
 }
 
 void coast(void)
 {
-	digitalWrite(h_bridge_q[0], HIGH);
-	digitalWrite(h_bridge_q[1], HIGH);
+	digitalWrite(Q0, HIGH);
+	digitalWrite(Q1, HIGH);
 }
